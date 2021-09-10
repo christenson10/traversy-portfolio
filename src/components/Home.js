fc2105723980fe50c8ba42d945b70b13523111db
Alex Christenson <christenson10@gmail.com>
2021-09-09 22:52:21 -0400

    101

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,17 @@
-import React from "react"
+import React from "react";
+import image from "../black-and-white-mountain-photo-l.jpg";
 
 export default function Home() {
-    return <h1>Home Page!</h1>
+  return (
+    <main>
+      <img
+        src={image}
+        alt="black and white mountains"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-yellow-300 font-bold leading-none lg:leading-snug home-name">Welcome.</h1>
+      </section>
+    </main>
+  );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,40 +4,40 @@ import { SocialIcon } from "react-social-icons";
 
 export default function NavBar() {
   return (
-    <header className="bg-gray-500">
-      <div className="container mx-auto flex justify-between">
+    <header className="bg-gray-400">
+      <div className="container mx-auto flex justify-between py-0">
         <nav className="flex">
           <NavLink
             to="/"
             exact
-            activeClassName="text-white"
-            className="inflex-flex items-center py-6 px-3 mr-4 text-black-300 hover:text-yellow-300 text-4xl font-bold tracking-widest"
+            activeClassName="text-black"
+            className="inline-flex items-center py-6 px-3 mr-4 text-black-300 hover:text-yellow-300 text-2xl font-bold tracking-widest"
           >
-            Alex
+            ALEX CHRISTENSON
           </NavLink>
           <NavLink
             to="post"
-            className="inflex-flex items-center py-3 px-3 my-4 rounded text-black-100 hover:text-yellow-300"
-            activeClassName="text-yellow-100 bg-yellow-300"
+            className="inline-flex items-center py-3 px-3 my-8 rounded text-black-100 hover:text-yellow-300 font-bold"
+            activeClassName="text-black-100 bg-yellow-300"
           >
             Blog Posts
           </NavLink>
           <NavLink
             to="project"
-            className="inflex-flex items-center py-3 px-3 my-4 rounded text-black-100 hover:text-yellow-300"
-            activeClassName="text-yellow-100 bg-yellow-300"
+            className="inline-flex items-center py-3 px-3 my-8 rounded text-black-100 hover:text-yellow-300 font-bold"
+            activeClassName="text-black-100 bg-yellow-300"
           >
             Projects
           </NavLink>
           <NavLink
             to="about"
-            className="inflex-flex items-center py-3 px-3 my-4 rounded text-black-100 hover:text-yellow-300"
-            activeClassName="text-yellow-100 bg-yellow-300"
+            className="inline-flex items-center py-3 px-3 my-8 rounded text-black-100 hover:text-yellow-300 font-bold"
+            activeClassName="text-black-100 bg-yellow-300"
           >
             About Me!
           </NavLink>
         </nav>
-        <div>
+        <div className="inline-flex py-3 px-3 my-6">
           <SocialIcon
             url="https://www.linkedin.com/in/christenson10/"
             className="mr-4"
@@ -61,6 +61,13 @@ export default function NavBar() {
           />
           <SocialIcon
             url="https://open.spotify.com/user/djohomum3yhyybr22q9s7ayry?si=c291da255cee4356"
+            className="mr-4"
+            target="-blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+                    <SocialIcon
+            url="mailto:christenson10@gmail.com"
             className="mr-4"
             target="-blank"
             fgColor="#fff"
