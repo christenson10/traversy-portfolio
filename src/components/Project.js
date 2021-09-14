97ee5028e54f156dca311a94a98bc525f93e7f96
Alex Christenson <christenson10@gmail.com>
2021-09-13 22:06:25 -0400

    projects page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,13 @@
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import sanityClient from "../client.js";
 
 export default function Project() {
-    const [projectData, setProjectData] = useState(null);
+  const [projectData, setProjectData] = useState(null);
 
-    useEffect(() => {
-        sanityClient.fetch(`*[_type == "project"]{
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "project"]{
             title,
             date,
             place,
@@ -13,9 +15,65 @@ export default function Project() {
             projectType,
             link,
             tags
-        }`).then((data) => setProjectData(data)).catch(console.error);
-    }, []);
-    
-    
-    return;
+        }`
+      )
+      .then((data) => setProjectData(data))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <main className="bg-green-100 min-h-screen p-12">
+      <section className="container mx-auto">
+        <h1 className="text-5xl flex justify-center cursive">Projects.</h1>
+        <h2 className="text-lg text-gray-600 flex justify-center mb-12">
+          Welcome to my projects page!
+        </h2>
+        <section className="grid grid-cols-2 gap-8">
+          {projectData &&
+            projectData.map((project, index) => (
+              <article className="relative rounded-lg shadow-xl bg-white p-16">
+                <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
+                  <a
+                    href={project.link}
+                    alt={project.title}
+                    target="blank"
+                    rel="nooopener noreferrer"
+                  >
+                    {project.title}
+                  </a>
+                </h3>
+                <div className="text-gray-500 text-xs space-x-4">
+                  <span>
+                    <strong className="font-bold">Finished on</strong>:{" "}
+                    {new Date(project.date).toLocaleDateString()}
+                  </span>
+                  <span>
+                    <strong className="font-bold">Company</strong>:
+                    {project.place}
+                  </span>
+                  <span>
+                    <strong className="font-bold">Type</strong>:{" "}
+                    {project.projectType}
+                  </span>
+                  <p className="my-6 text-lg text-gray-700 leading-relaxed">
+                    {project.description}
+                  </p>
+                  <a
+                    href={project.length}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="text-red-500 font-bold hover:underline hover:text-red-400 text-xl"
+                  >
+                    View The Project{" "}
+                    <span role="img" aria-label="right pointer">
+                      👉
+                    </span>
+                  </a>
+                </div>
+              </article>
+            ))}
+        </section>
+      </section>
+    </main>
+  );
 }
